054f49fae2364bae6df3b89d4e1bd3e8033d147b
Vazgen Davtyan <vazgenddavtyan@gmail.com>
2019-02-06 14:50:51 +0400

    Updates

---
+++ precache-manifest.3f659157c4e24d4ee7242d5f73994516.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8214f3c6ed3280d05a9a",
+    "url": "/temperature/static/js/runtime~main.8214f3c6.js"
+  },
+  {
+    "revision": "3ca18b67cf3b917e09c4",
+    "url": "/temperature/static/js/main.3ca18b67.chunk.js"
+  },
+  {
+    "revision": "cb9759c246ec60ffb23a",
+    "url": "/temperature/static/js/1.cb9759c2.chunk.js"
+  },
+  {
+    "revision": "3ca18b67cf3b917e09c4",
+    "url": "/temperature/static/css/main.1d7b8450.chunk.css"
+  },
+  {
+    "revision": "1fa70b1dc51d99c75a6a1612830db3a4",
+    "url": "/temperature/index.html"
+  }
+];
